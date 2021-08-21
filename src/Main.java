ea8051046abf6c01ed331c64ebe4cbeaced96f77
Mikhail Gozhenko <sacred9900@gmail.com>
2021-08-21 13:18:38 +0900

    Change if/else

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-    public class Main {
-        public static void main(String[] args) {
-            int initialAccount = 100; // начальный баланс
-            int replenishmentAmount = 999; //сумма пополнения
-            int balans = initialAccount + replenishmentAmount; // баланс счёта
+public class Main {
+    public static void main(String[] args) {
+        int initialAccount = 100; // начальный баланс
+        int replenishmentAmount = 999; //сумма пополнения
 
-            if (replenishmentAmount > 1000) { int bonus =  replenishmentAmount / 100;//если сумма пополнения больше 1000
-                System.out.println("Ваш бонус составляет "  + bonus +" р.") ;
-            }
-            if (replenishmentAmount <= 1000) {int bonus = 0;  // а если сумма пополнения равна или меньше 1000, то бонус ноль
-                System.out.println("Ваш бонус составляет "  + bonus +" р."); }}}
+        int bonus;
+        if (replenishmentAmount > 1000) {
+            bonus = (replenishmentAmount / 100); //если сумма пополнения больше 1000, то высчитывается бонус
+
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Ваш бонус составляет " + bonus + " р.");
+    }
+}
+
